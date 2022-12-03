5eb85c08effc2b0f4212a479db58b289264b63bf
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-03 00:51:20 -0800

    changed connection to a remote database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-3', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.be7xz0l.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

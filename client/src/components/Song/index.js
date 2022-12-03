e3d297b910b9491546d27fad00657c05aa2bd07e
wwartick <willwartick@gmail.com>
2022-12-03 15:30:13 -0800

    remerge with develop

--- client/src/components/Song/index.js
+++ client/src/components/Song/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 /* TODO: !!! */
 function Song() {
+  const [state, dispatch] = useStoreContext();
+  
   const addToCart = () => {
-    console.log('add to cart');
-  }
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...item, purchaseQuantity: 1 }
+    });
+  };
 
   return (
     <div className='basis-1/4 p-1'>

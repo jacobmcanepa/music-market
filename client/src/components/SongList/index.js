91ed542a50b0eed999bc2636424b1cdd134ab421
wwartick <willwartick@gmail.com>
2022-12-03 16:56:47 -0800

    seed file + home page loading db data(code there, data not loading though

--- client/src/components/SongList/index.js
+++ client/src/components/SongList/index.js
@@ -23,11 +23,8 @@ function SongList() {
       data.songs.forEach((song) => {
         idbPromise('songs', 'put', song);
       });
-      // add else if to check if `loading` is undefined in `useQuery()` Hook
     } else if (!loading) {
-      // since we're offline, get all of the data from the `songs` store
       idbPromise('songs', 'get').then((songs) => {
-        // use retrieved data to set global state for offline browsing
         dispatch({
           type: UPDATE_SONGS,
           songs: songs
@@ -82,7 +79,6 @@ function SongList() {
               _id={song._id}
               name={song.name}
               price={song.price}
-              quantity={song.quantity}
             />
           ))}
         </div>
